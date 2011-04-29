d557eae42c405445390608dceb4fb355e6016de8
Chris Mueller <christoph.mueller@dfki.de>
2011-04-29 09:12:33 +0200

    prepare feature representation for visual detectors

---
+++ avalon_base/feature.h
@@ -0,0 +1,45 @@
+/*
+ * Avalon base - feature.h
+ * definition for feature representation for several visual computervision algorithm
+ * 
+ * created by: 
+ *      Thomas Grassow <deshalb@informatik.uni-bremen.de>
+ *      Christoph Mueller <christoph.mueller@dfki.de>
+ */
+
+#ifndef AVALON_BASE__FEATURE_H_
+#define AVALON_BASE__FEATURE_H_
+
+#include <base/eigen.h>
+
+namespace avalon {
+namespace feature {
+
+/**
+ * Simple structure for representing a buoy found in an image 
+ */
+struct Buoy
+{
+        /** x-position of the buoy in image in pixel */
+    	int image_x;
+
+        /** y-position of the buoy in image in pixel */
+    	int image_y;
+
+        /** radius of the buoy in in image in pixel */
+	int image_radius;
+	
+        /** propability that this is a real buoy in image */
+        double propability;
+	
+        /** real world coordinates of the buoy */
+	base::Vector3d world_coord;
+
+	Buoy(int x, int y, int r)
+	: image_x(x), image_y(y), image_radius(r), propability(1) {}
+};
+
+}} // namespace avalon::feature
+
+#endif // AVALON_BASE__FEATURE_H_
+
